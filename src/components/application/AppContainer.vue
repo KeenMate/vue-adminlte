0734ddfed628bfb6feba12e9d9c830d7bd0decde
Filip Jakab <JakabFilip.FJ@gmail.com>
2020-05-26 01:20:22 +0200

    Corrected classes to prevent additional sidebar padding when its not needed

--- src/components/application/AppContainer.vue
+++ src/components/application/AppContainer.vue
@@ -1,21 +1,29 @@
 <template>
 	<div :class="bodyStyles">
 		<div class="wrapper">
-			<slot name="top-nav"></slot>
-			<slot name="side-nav"></slot>
+			<top-navigation v-if="$scopedSlots['top-nav']">
+				<slot name="top-nav"></slot>
+			</top-navigation>
+			<sidenav v-if="$scopedSlots['side-nav']">
+				<slot name="side-nav"></slot>
+			</sidenav>
 			<slot></slot>
 		</div>
 	</div>
 </template>
 
 <script>
+import TopNavigation from "../navigation/TopNavigation"
+import Sidenav from "../navigation/Sidenav"
+
 export default {
 	name: "AppContainer",
+	components: {Sidenav, TopNavigation},
 	props: {},
 	computed: {
 		bodyStyles() {
 			return [
-				"sidebar-mini"
+				this.$scopedSlots["side-nav"] && "sidebar-mini"
 			].filter(x => x).join(" ")
 		}
 	}
