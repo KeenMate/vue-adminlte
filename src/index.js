c8be21213741c6666cec6af39c6acdaf169c0acd
Filip Jakab <JakabFilip.FJ@gmail.com>
2020-05-25 20:29:52 +0200

    Fix of plugin component install

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
-export default {
-	install: Vue => {
-		// some logic can be placed here
-	},
-	version: "__VERSION__"
+// import "AdminLTE/dist/css/adminlte.css"
+// import "AdminLTE/build/scss/AdminLTE-condensed.scss"
+// import "./vendors/adminlte/dist/css/adminlte.css"
+// import "./vendors/adminlte/build/scss/AdminLTE-condensed.scss"
+
+import * as components from "./components"
+
+const version = "__VERSION__"
+
+const install = Vue => {
+	Object
+		.keys(components)
+		.forEach(name => {
+			Vue.component(name, components[name])
+		})
+}
+const plugin = {
+	install,
+	version
 }
 
-export * from "./components"
+export default plugin
+
+if (typeof window !== "undefined" && window.Vue)
+	window.Vue.use(plugin)
