c49876ddd3849f95fbc04644bd907d13bd040972
Filip Jakab <JakabFilip.FJ@gmail.com>
2020-06-03 00:32:51 +0200

    Fix of initial value for `v-model`

--- src/components/forms/SummernoteEditor.vue
+++ src/components/forms/SummernoteEditor.vue
@@ -9,25 +9,55 @@ import "../../vendors/adminlte/plugins/summernote/summernote-bs4.min"
 
 import $ from "jquery"
 
+/**
+ * @description Wrapper for summernote editor. Works with `v-model`
+ */
 export default {
 	name: "SummernoteEditor",
+	props: {
+		/**
+		 * @type {string}
+		 * @description Value to be passed to editor
+		 */
+		value: String
+	},
 	mounted() {
-		$(this.$refs.textarea).summernote({
-			codemirror: {
-				theme: "monokai"
-			},
-			callbacks: {
-				onChange: this.onSummernoteChange
-			}
-		})
+		this.initSummernoteEditor()
 	},
 	beforeDestroy() {
-		$(this.$refs.textarea).summernote("destroy")
+		this.$editor = $(this.$refs.textarea).summernote("destroy")
+	},
+	watch: {
+		value: {
+			immediate: true,
+			handler(val) {
+				const setter = () => this.$editor.summernote("code", val)
+
+				if (this.$editor) {
+					const currentVal = this.$editor.summernote("code")
+					if (currentVal !== val)
+						setter()
+				} else {
+					this.initSummernoteEditor()
+					this.$nextTick().then(() => setter())
+				}
+			}
+		}
 	},
 	methods: {
+		initSummernoteEditor() {
+			this.$editor = $(this.$refs.textarea).summernote({
+				callbacks: {
+					onChange: this.onSummernoteChange
+				}
+			})
+		},
 		onSummernoteChange(content) {
 			this.$emit("input", content)
 		}
+	},
+	data: {
+		$editor: null
 	}
 }
 </script>
