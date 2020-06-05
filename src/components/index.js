91dff1668b044d31240f175588a97c3741be54c9
Filip Jakab <JakabFilip.FJ@gmail.com>
2020-06-05 21:27:43 +0200

    Added markdown editor to exports

--- src/components/index.js
+++ src/components/index.js
@@ -15,6 +15,7 @@ export {default as LteSwitch} from "./forms/LteSwitch.vue"
 export {default as Radio} from "./forms/Radio.vue"
 export {default as RadioGroup} from "./forms/RadioGroup.vue"
 export {default as SummernoteEditor} from "./forms/SummernoteEditor.vue"
+export {default as MarkdownEditor} from "./forms/MarkdownEditor.vue"
 export {default as UploadFile} from "./forms/UploadFile.vue"
 export {default as validatedInputProps} from "./forms/validatedInputProps"
 
