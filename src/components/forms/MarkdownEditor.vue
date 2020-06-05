189e864f9e6a0b8089660dc60eeaf34ab4b91234
Filip Jakab <JakabFilip.FJ@gmail.com>
2020-06-05 20:57:47 +0200

    Added MarkdownEditor.vue wrapping VueEasymde package

---
+++ src/components/forms/MarkdownEditor.vue
@@ -0,0 +1,27 @@
+<template>
+	<vue-easymde />
+</template>
+
+<script>
+import VueEasymde from "vue-easymde"
+
+export default {
+	name: "MarkdownEditor",
+	components: {
+		VueEasymde
+	}
+}
+</script>
+
+<style lang="scss">
+	@import "~easymde/dist/easymde.min.css";
+
+	.editor-toolbar {
+		border-radius: 0 !important;
+		padding: 0 !important;
+
+	&:before, &:after {
+		           margin: 0 !important;
+	           }
+	}
+</style>
