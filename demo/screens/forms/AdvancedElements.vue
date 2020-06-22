639cd5bb75f764a851f4d91317b34d4517075e2b
ZelvaMan <zelvaman16@gmail.com>
2020-06-22 19:27:45 +0200

    dual list styling

--- demo/screens/forms/AdvancedElements.vue
+++ demo/screens/forms/AdvancedElements.vue
@@ -3,116 +3,121 @@
     <!-- 
 									kdys se zmeni  nebo track-by u multiselect tak prestane fungovat
     -->
-    <card title="Vue-Multiselect default" is-info>
-      <!-- -->
-      <div class="row">
-        <!-- Single select with search -->
-        <div class="col-md-6">
-          <label>Single select with search</label>
-          <multiselect
-            v-model="singleselectvalue"
-            :options="SelectOptions"
-            :close-on-select="true"
-            placeholder="Select one"
-            label="name"
-            track-by="name"
-          >Single select with search</multiselect>
+    <section class="content-header">
+      <h1>Advanced Elements</h1>
+    </section>
+    <section class="content">
+      <card title="Vue-Multiselect default" is-info>
+        <!-- -->
+        <div class="row">
+          <!-- Single select with search -->
+          <div class="col-md-6">
+            <label>Single select with search</label>
+            <multiselect
+              v-model="singleselectvalue"
+              :options="SelectOptions"
+              :close-on-select="true"
+              placeholder="Select one"
+              label="name"
+              track-by="name"
+            >Single select with search</multiselect>
+          </div>
+          <!-- Multiselect with seatch-->
+          <div class="col-md-6">
+            <label>Multiselect with seatch</label>
+            <multiselect
+              v-model="multiselectValue"
+              :options="SelectOptions"
+              :multiple="true"
+              :close-on-select="true"
+              placeholder="Pick some"
+              label="name"
+              track-by="name"
+            ></multiselect>
+          </div>
         </div>
-        <!-- Multiselect with seatch-->
-        <div class="col-md-6">
-          <label>Multiselect with seatch</label>
-          <multiselect
-            v-model="multiselectValue"
-            :options="SelectOptions"
-            :multiple="true"
-            :close-on-select="true"
-            placeholder="Pick some"
-            label="name"
-            track-by="name"
-          ></multiselect>
+        <!-- -->
+        <div class="row">
+          <!-- singleselect disabled -->
+          <div class="col-md-6">
+            <label>singleselect disabled</label>
+            <multiselect
+              v-model="singleselectvalue"
+              :options="SelectOptions"
+              :close-on-select="true"
+              :clear-on-select="false"
+              placeholder="Select one"
+              label="name"
+              track-by="name"
+              disabled
+            ></multiselect>
+          </div>
+          <!-- Multu select with seatch and one of options disabled-->
+          <div class="col-md-6">
+            <label>Multi select with seatch and one of options disabled</label>
+            <multiselect
+              v-model="multiselectValue"
+              :options="SelectOptionsWdisabled"
+              :multiple="true"
+              :close-on-select="true"
+              placeholder="Pick some"
+              label="name"
+              track-by="name"
+            ></multiselect>
+          </div>
         </div>
-      </div>
-      <!-- -->
-      <div class="row">
-        <!-- singleselect disabled -->
-        <div class="col-md-6">
-          <label>
-            <b>NOTWORKING</b> singleselect disabled
-          </label>
-          <multiselect
-            v-model="singleselectvalue"
-            :options="SelectOptions"
-            :close-on-select="true"
-            :clear-on-select="false"
-            placeholder="Select one"
-            label="name"
-            track-by="name"
-            disabled
-          ></multiselect>
+        <!-- -->
+        <div class="row">
+          <div class="col-md-6">
+            <label>Single select with search and blue text and red background</label>
+          </div>
+          <div class="col-md-6">
+            <label>Multi select with search and yelow text and red background</label>
+          </div>
         </div>
-        <!-- Multu select with seatch and one of options disabled-->
-        <div class="col-md-6">
-          <label>Multi select with seatch and one of options disabled</label>
-          <multiselect
-            v-model="multiselectValue"
-            :options="SelectOptionsWdisabled"
-            :multiple="true"
-            :close-on-select="true"
-            placeholder="Pick some"
-            label="name"
-            track-by="name"
-          ></multiselect>
+        <div class="row">
+          <!-- Single select with search and blue text-->
+          <div class="col-md-6">
+            <multiselect
+              v-model="singleselectvalue"
+              :options="SelectOptions"
+              :close-on-select="true"
+              placeholder="Select one"
+              label="name"
+              track-by="name"
+              class="has-text-danger"
+              style=" background:red; color: blue;"
+            ></multiselect>
+          </div>
+          <!-- Multu select with seatch-->
+          <div class="col-md-6">
+            <multiselect
+              v-model="multiselectValue"
+              :options="SelectOptions"
+              :multiple="true"
+              :close-on-select="true"
+              placeholder="Pick some"
+              label="name"
+              track-by="name"
+              class="has-text-success"
+              style=" background:red; color: yellow;"
+            ></multiselect>
+          </div>
         </div>
-      </div>
-      <!-- -->
-      <div class="row">
-        <div class="col-md-6">
-          <label>Single select with search and blue text and red background</label>
+      </card>
+      <card title="dual listbox" is-info>
+        <div class="row">
+          <div class="col-12">
+            <dualListBox
+              :list1="ListBox1"
+              :list2="ListBox2"
+              title="Dual list box"
+              id="dualListBox"
+            >DualListBox</dualListBox>
+          </div>
         </div>
-        <div class="col-md-6">
-          <label>Multi select with search and yelow text and red background</label>
-        </div>
-      </div>
-      <div class="row">
-        <!-- Single select with search and blue text-->
-        <div class="col-md-6">
-          <multiselect
-            v-model="singleselectvalue"
-            :options="SelectOptions"
-            :close-on-select="true"
-            placeholder="Select one"
-            label="name"
-            track-by="name"
-            class="has-text-danger"
-            style=" background:red; color: blue;"
-          ></multiselect>
-        </div>
-        <!-- Multu select with seatch-->
-        <div class="col-md-6">
-          <multiselect
-            v-model="multiselectValue"
-            :options="SelectOptions"
-            :multiple="true"
-            :close-on-select="true"
-            placeholder="Pick some"
-            label="name"
-            track-by="name"
-            class="has-text-success"
-            style=" background:red; color: yellow;"
-          ></multiselect>
-        </div>
-      </div>
-    </card>
-    <card title="dual listbox" is-info>
-      <div class="row">
-        <dualListBox
-          :list1="ListBox1"
-          :list2="ListBox2"
-          title="Dual list box"
-          id="dualListBox"
-        >DualListBox</dualListBox>
-      </div>
-    </card>
+      </card>
+    </section>
   </page>
 </template>
 
--- src/components/forms/DualListBox.vue
+++ src/components/forms/DualListBox.vue
@@ -1,28 +1,105 @@
 <template>
-  <div class="input-group input-group-sm">
-    <div class="row">{{title}}</div>
-    <div class="row">
-      <div class="col-md-5">
-        <div class="select is-multiple">
-          <select multiple size="8" id="leftSelect" @change="oneToRight">
-            <option v-for="item1 in list1">{{item1}}</option>
-          </select>
+  <!-- <div class="form-group">
+    <label>{{title}}</label>
+    <div class="bootstrap-duallistbox-container row moveonselect moveondoubleclick">
+      <div class="box1 col-md-6">
+        <div class="btn-group buttons flex-fill">
+          <button class="btn moveall btn-outline-secondary" @click="allToRight" title="move all">>></button>
         </div>
+        <select
+          multiple
+          size="8"
+          id="leftSelect"
+          @change="oneToRight"
+          class="d-flex align-items-stretch flex-grow-1"
+        >
+          <option v-for="item1 in list1">{{item1}}</option>
+        </select>
       </div>
-      <div class="col-md-2">
-        <div class="buttons are-primary">
-          <button class="button" @click="oneToRight">></button>
-          <button class="button" @click="allToRight">>></button>
-          <button class="button" @click="oneToLeft">&lt;</button>
-          <button class="button" @click="allToLeft">&lt;&lt;</button>
+      <!--<div class="buttons are-primary">
+        <button class="button" @click="oneToRight">></button>
+        <button class="button" @click="allToRight">>></button>
+        <button class="button" @click="oneToLeft">&lt;</button>
+        <button class="button" @click="allToLeft">&lt;&lt;</button>
+      </div>
+      <div class="box2 col-md-6">
+        <div class="btn-group buttons">
+          <button class="btn removeall btn-outline-secondary" @click="allToLeft">&lt;&lt;</button>
         </div>
-      </div>
-      <div class="col-md-5">
-        <select multiple size="8" id="rightselect" @change="oneToLeft">
+        <select
+          multiple
+          size="8"
+          id="rightselect"
+          @change="oneToLeft"
+          class="d-flex align-items-stretch flex-grow-1"
+        >
           <option v-for="item2 in list2">{{item2}}</option>
         </select>
       </div>
     </div>
+  </div>-->
+  <div class="form-group">
+    <label>Multiple</label>
+    <div class="bootstrap-duallistbox-container row moveonselect moveondoubleclick">
+      <div class="box1 col-md-6">
+        <label for="bootstrap-duallistbox-nonselected-list_" style="display: none;"></label>
+        <span class="info-container">
+          <span class="info">Showing all 6</span>
+          <button type="button" class="btn btn-sm clear1" style="float:right!important;">show all</button>
+        </span>
+        <input class="form-control filter" type="text" placeholder="Filter" />
+        <div class="btn-group buttons">
+          <button type="button" class="btn moveall btn-outline-secondary" title="Move all">&gt;&gt;</button>
+        </div>
+        <select
+          multiple="multiple"
+          id="bootstrap-duallistbox-nonselected-list_"
+          name="_helper1"
+          style="height: 102px;"
+          class="form-control"
+        >
+          <option selected>Alabama</option>
+          <option>California</option>
+          <option>Delaware</option>
+          <option>Tennessee</option>
+          <option>Texas</option>
+          <option>Washington</option>
+        </select>
+      </div>
+      <div class="box2 col-md-6">
+        <label for="bootstrap-duallistbox-selected-list_" style="display: none;"></label>
+        <span class="info-container">
+          <span class="info">Showing all 1</span>
+          <button type="button" class="btn btn-sm clear2" style="float:right!important;">show all</button>
+        </span>
+        <input class="form-control filter" type="text" placeholder="Filter" />
+        <div class="btn-group buttons">
+          <button
+            type="button"
+            class="btn removeall btn-outline-secondary"
+            title="Remove all"
+          >&lt;&lt;</button>
+        </div>
+        <select
+          multiple="multiple"
+          id="bootstrap-duallistbox-selected-list_"
+          name="_helper2"
+          style="height: 102px;"
+          class="form-control"
+        >
+          <option data-sortindex="21">Alaska</option>
+        </select>
+      </div>
+    </div>
+    <select class="duallistbox" multiple="multiple" style="display: none;">
+      <option selected>Alabama</option>
+      <option data-sortindex="21">Alaska</option>
+      <option>California</option>
+      <option>Delaware</option>
+      <option>Tennessee</option>
+      <option>Texas</option>
+      <option>Washington</option>
+    </select>
   </div>
 </template>
 
