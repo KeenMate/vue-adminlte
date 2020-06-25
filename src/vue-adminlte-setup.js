9616600129aa7e09ea2cd00dede06d7ac179dba5
Filip Jakab <JakabFilip.FJ@gmail.com>
2020-06-25 18:37:28 +0200

    link fix

--- src/vue-adminlte-setup.js
+++ src/vue-adminlte-setup.js
@@ -3,4 +3,4 @@ import "./vendors/adminlte/js/adminlte.esm"
 
 // styles
 import "./vendors/adminlte/css/adminlte.min.css"
-import "@keenmate/vue-adminlte/src/assets/keenmate/image.css"
+import "./assets/keenmate/image.css"
