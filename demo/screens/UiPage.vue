1d3bbb330c905c600833a8445d0de32e0b3d162a
Filip Jakab <jakabfilip.fj@gmail.com>
2020-09-15 23:20:51 +0200

    Fixed test of alert

--- demo/screens/UiPage.vue
+++ demo/screens/UiPage.vue
@@ -242,19 +242,12 @@
 					<div class="card-body">
 						<alert status="danger" closable>
 							<template #header>
-								<i class="icon fas fa-ban"></i> Alert!
+								<h5><i class="icon fas fa-ban"></i> Alert!</h5>
 							</template>
 							Danger alert preview. This alert is dismissable. A wonderful serenity has taken possession of my
 							entire
 							soul, like these sweet mornings of spring which I enjoy with my whole heart.
 						</alert>
-						<!--<div class="alert alert-danger alert-dismissible">
-							<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-							<h5><i class="icon fas fa-ban"></i> Alert!</h5>
-							Danger alert preview. This alert is dismissable. A wonderful serenity has taken possession of my
-							entire
-							soul, like these sweet mornings of spring which I enjoy with my whole heart.
-						</div>-->
 						<div class="alert alert-info alert-dismissible">
 							<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
 							<h5><i class="icon fas fa-info"></i> Alert!</h5>
