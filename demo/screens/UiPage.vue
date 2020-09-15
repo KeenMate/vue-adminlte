bb30e056e53d92022b621fda9a494b713b2b9cb9
Filip Jakab <jakabfilip.fj@gmail.com>
2020-09-15 23:14:08 +0200

    Added `Alert` component

--- demo/screens/UiPage.vue
+++ demo/screens/UiPage.vue
@@ -240,13 +240,21 @@
 					</div>
 					<!-- /.card-header -->
 					<div class="card-body">
-						<div class="alert alert-danger alert-dismissible">
+						<alert status="danger" closable>
+							<template #header>
+								<i class="icon fas fa-ban"></i> Alert!
+							</template>
+							Danger alert preview. This alert is dismissable. A wonderful serenity has taken possession of my
+							entire
+							soul, like these sweet mornings of spring which I enjoy with my whole heart.
+						</alert>
+						<!--<div class="alert alert-danger alert-dismissible">
 							<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
 							<h5><i class="icon fas fa-ban"></i> Alert!</h5>
 							Danger alert preview. This alert is dismissable. A wonderful serenity has taken possession of my
 							entire
 							soul, like these sweet mornings of spring which I enjoy with my whole heart.
-						</div>
+						</div>-->
 						<div class="alert alert-info alert-dismissible">
 							<button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
 							<h5><i class="icon fas fa-info"></i> Alert!</h5>
---
+++ src/components/ui/Alert.vue
@@ -0,0 +1,45 @@
+<template>
+	<div :class="alertStyles">
+		<button v-if="closable" type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+		<slot v-if="$scopedSlots.header" name="header"></slot>
+		<h5 v-else>{{title}}</h5>
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Alert",
+	props: {
+		/**
+		 * Title of alert component
+		 * @type {string}
+		 */
+		title: String,
+
+		/**
+		 * Determines the color of displayed alert
+		 */
+		status: String,
+
+		/**
+		 * Controls whether to display 'close' button
+		 * @type {boolean}
+		 */
+		closable: Boolean
+	},
+	computed: {
+		alertStyles() {
+			return [
+				"alert",
+				this.status && `alert-${this.status}`,
+				this.closable && "alert-dismissible"
+			]
+		}
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
