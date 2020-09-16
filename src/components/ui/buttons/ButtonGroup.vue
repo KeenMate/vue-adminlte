5dcef49d7d46d5b21e98e22484fb095b3bd40513
Filip Jakab <JakabFilip.FJ@gmail.com>
2020-09-16 22:09:32 +0200

    Added ButtonGroup.vue component.

---
+++ src/components/ui/buttons/ButtonGroup.vue
@@ -0,0 +1,26 @@
+<template>
+	<div :class="styles">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+  name: "ButtonGroup",
+	props: {
+  	isFlat: Boolean
+	},
+	computed: {
+  	styles() {
+  		return [
+  			"button-grp",
+			  this.isFlat && "btn-flat"
+		  ]
+	  }
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
