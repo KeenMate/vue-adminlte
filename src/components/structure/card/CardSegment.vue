9c7e299a5463fe1c62ece4daf5159338fec0a29b
Filip Jakab <jakabfilip.fj@gmail.com>
2020-10-18 14:32:01 +0200

    Added new card component for rendering part of card that is divided from both top and bottom by gray line.

---
+++ src/components/structure/card/CardSegment.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="card-segment">
+		<slot></slot>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "CardSegment"
+}
+</script>
+
+<style lang="scss">
+.card-segment {
+	border-top: 1px solid #dee2e6;
+	border-bottom: 1px solid #dee2e6;
+}
+</style>
